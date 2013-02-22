0e1a65a449d119a2247b3c6410d36d6ef416d952
fakestarbaby <fakestarbaby@gmail.com>
2013-02-22 17:19:18 +0900

    Added rails new . -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleSpring::Application.config.secret_token = '56835f36d9d6b4379267ea732ddc56ae728ce02a2c64923eb56928beb72a6335ecac42bb5e35224b8884b8e1d6774786290744ec444676611c5f01fa4b5ae5f2'
